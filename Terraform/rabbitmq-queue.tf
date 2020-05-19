67c388dcc6de6097fdf275f0930e4d56dd089d46
Andreas Mosti <andreas.mosti@gmail.com>
2020-05-19 20:29:36 +0200

    terraform

---
+++ Terraform/rabbitmq-queue.tf
@@ -0,0 +1,32 @@
+provider "rabbitmq" {
+  endpoint = var.rabbitmq_hostname
+  username = var.rabbitmq_username
+  password = var.rabbitmq_password
+}
+
+resource "rabbitmq_vhost" "test" {
+  name = "test"
+}
+
+resource "rabbitmq_permissions" "guest" {
+  user  = "guest"
+  vhost = "${rabbitmq_vhost.test.name}"
+
+  permissions {
+    configure = ".*"
+    write     = ".*"
+    read      = ".*"
+  }
+}
+
+resource "rabbitmq_queue" "hello" {
+  name  = "hello"
+  vhost = "${rabbitmq_permissions.guest.vhost}"
+
+  settings {
+    durable     = false
+    auto_delete = true
+  }
+}
+
+
---
+++ Terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "rabbitmq_hostname" {
+  type = string
+  default = "http://127.0.0.1"
+}
+
+variable "rabbitmq_username" {
+  type = string
+  default = "guest"
+}
+
+variable "rabbitmq_password" {
+  type = string
+  default = "guest"
+}
